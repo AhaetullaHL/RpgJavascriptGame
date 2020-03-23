22a60c3b1db0563b446514165b4af5f350e0a821
tristantheb <tristantheb@gmail.com>
2020-03-23 17:14:23 +0100

    First commit

---
+++ combatGame.js
@@ -0,0 +1,37 @@
+/**
+ * @author Hermann & Tristan
+ * @copyright 2020
+ * @name RolePlayGame
+ * @version 1.0.0
+ */
+
+/**
+ * @since 1.0.0
+ * 
+ * @function character() - Create a new caracter
+ * 
+ * @param {string} name - Set the caracter name
+ * @param {string} type - Set the caracter class
+ * @param {array} inventoryList - Set the list of inventory
+ * @param {number} [life=100] - Set the start life level
+ * @param {number} [isLiving=true] - Set the default status of life (living/dead)
+ * 
+ * @returns {object} Return a new object of character
+ */
+function character(name, type, inventoryList, life = 100, isLiving = true) {
+  let char = {};
+  return char;
+}
+
+/**
+ * @since 1.0.0
+ * 
+ * @function startGame() Start the game
+ * @param {var} firstChar Your first character
+ * @param {var} secondChar Your second character
+ * 
+ * @callback Console.log() Write in the console
+ */
+function startGame(firstChar, secondChar) {
+ // Code ...
+}

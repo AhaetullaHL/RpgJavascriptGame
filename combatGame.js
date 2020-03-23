08f0130c4a11c5640ca77c3a4f1960d88b4d0857
tristantheb <tristantheb@gmail.com>
2020-03-23 18:55:34 +0100

    New function for creating items

--- combatGame.js
+++ combatGame.js
@@ -12,15 +12,30 @@
  * 
  * @param {string} name - Set the caracter name
  * @param {string} type - Set the caracter class
- * @param {array} inventoryList - Set the list of inventory
+ * @param {array} [inventoryList=[]] - Set the list of inventory
  * @param {number} [life=100] - Set the start life level
  * @param {number} [isLiving=true] - Set the default status of life (living/dead)
  * 
  * @returns {object} Return a new object of character
  */
-function character(name, type, inventoryList, life = 100, isLiving = true) {
+function character(name, type, inventoryList = [], life = 100, isLiving = true) {
   let char = {};
   return char;
+}
+
+/**
+ * @since 1.0.0
+ * 
+ * @function itemCreate() - Create a new item
+ * 
+ * @param {string} name - The name of your item
+ * @param {string} description - An beautiful description
+ * @param {number} durability - Durability of the item, decrease when used
+ * @param {boolean} [isConssumable=false] - This item is conssumable ?
+ * @param {boolean} [isQuestItem=false] - This item is a quest item ?
+ */
+function itemCreate(name, description, durability, isConssumable = false, isQuestItem = false) {
+  // Code ...
 }
 
 /**
